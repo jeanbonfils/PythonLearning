706ee0344cebb8ad34ffb72516d7bcbd2a778308
jeanbonfils <115049529+jeanbonfils@users.noreply.github.com>
2022-10-05 02:05:18 +0200

    Python code to access string index

---
+++ accessindex.py
@@ -0,0 +1,14 @@
+city="Learning Python"
+print("Printing string value: ",city)
+print("Printing from zero index to last value: ",city[0:])
+print("Printing first index of the string")
+print(city[0])
+
+print("#Printing second index of the string")
+print(city[1])
+
+print("#Printing 2nd last index of the string")
+print(city[-2])
+
+print("#Printing last index of the string")
+print(city[-1])
